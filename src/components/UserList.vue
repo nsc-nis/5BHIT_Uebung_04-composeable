838c6627b956a7acc1d70d1c8726c3344aee212b
NIS <niklas.schachl@htl-wels.at>
2022-11-07 09:18:37 +0000

    Create UserList.vue, implemented loading of JSON-File (external)

---
+++ src/components/UserList.vue
@@ -0,0 +1,19 @@
+<template> </template>
+
+<script>
+import { useFetch } from "../composeables/UseFetch.js";
+export default {
+  setup() {
+    const { data, error, loading } = useFetch(
+      "https://jsonplaceholder.typicode.com/users",
+      {}
+    );
+
+    return {
+      data,
+      error,
+      loading
+    };
+  }
+};
+</script>
