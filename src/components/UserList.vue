b5cf4d9eb1c922d60c7266769e79313683f9573f
NIS <niklas.schachl@htl-wels.at>
2022-11-07 09:26:32 +0000

    Implemented showing the data

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,4 +1,28 @@
-<template> </template>
+<template>
+    <div v-if="error">  <!-- if(error != null) -->
+        <h2>Error: {{ error }}</h2>
+    </div>
+    <div v-else>
+        <div v-if="loading">
+            <h2>Loading data...</h2>
+        </div>
+        <h1>Users</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in data">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.username }}</td>
+                </tr>
+            </tbody>
+    </div>
+        
+</template>
 
 <script>
 import { useFetch } from "../composeables/UseFetch.js";
